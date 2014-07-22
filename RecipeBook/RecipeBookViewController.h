9d9b98a44ea3bc491c0d69a5d1488352ada88efa
Elvis Lee <elvis.lee@movial.com>
2014-07-22 17:03:25 +0800

    Add a segue.

--- RecipeBook/RecipeBookViewController.h
+++ RecipeBook/RecipeBookViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RecipeBookViewController : UIViewController
+@interface RecipeBookViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
